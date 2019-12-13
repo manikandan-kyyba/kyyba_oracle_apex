292ec5de8f78d5b0420b4d4650020d115345e0d9
Manikandan-Kyyba <manikandana@kyyba.com>
2019-12-13 23:25:55 +0530

    ok

---
+++ DATA_TABLE/Select_Query.sql
@@ -0,0 +1,6 @@
+
+SELECT distinct "TAO#" d, "TAO#" v FROM "BackTAODirectoRegion";
+
+SELECT distinct "RegionalMgr" d, "RegionalMgr" v FROM "BackTAODirectoRegion";
+
+SELECT distinct "Assignment#" d, "Assignment#" v FROM "tblListAssignmentNumber";
